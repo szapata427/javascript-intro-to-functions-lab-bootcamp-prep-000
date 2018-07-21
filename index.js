57bf8ca086fa43771c7bda67c205f91b54eef8e7
Steven Zapata <szapata427@gmail.com>
2018-07-21 02:19:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
-  return "I can\'t hear you!"
+  return 'I can\'t hear you!'
 }
 
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase())
-  return "YES INDEED!"
+  return 'YES INDEED!'
 }
 
 function sayHiToGrandma(string) {
